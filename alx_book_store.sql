dc26b42819d2a1b40cfa80f3b12749f71bff7e78
Reagan-dev <reagansimiyu38@gmail.com>
2025-06-28 18:28:23 +0300

    added a new file

---
+++ alx_book_store.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS alx_book_store;
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+CREATE TABLE Authors (
+    author_id INT,
+    author_name VARCHAR(215) NOT NULL,
+    PRIMARY KEY (author_id)
+);
+CREATE TABLE Books (
+	book_id INT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+
+CREATE TABLE Customers (
+    customer_id INT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE Orders (
+    order_id INT,
+    customer_id INT,
+    order_date DATE,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Order_Details (
+    orderdetailid INT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
